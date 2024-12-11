c4c898fa657ccfd03c08c3abde7d2283eb2a0359
Paul Chukwuemeka <paulchukwuemeka123@gmail.com>
2024-12-11 16:13:13 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -11,26 +11,60 @@ import {
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+const regex =
+  /^([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])){3}$/;
 let info = null;
 function App() {
   const [ipAddress, setIpAddress] = useState("");
   const [result, setResult] = useState(null);
+  const [input, setInput] = useState("");
   const apiKey =
-    "at_WdHFxk0MfhURjtsOD2o52dzi9IgX8";
+    "at_Udf8af3TfIQauqxQLPuFb4VjEpj3C";
 
-  // const apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`;
-  // useEffect(() => {
-  //   fetch(apiUrl)
-  //     .then(async (res) => await res.json())
-  //     .then(async (data) => {
-  //       await setResult(data);
-  //     })
-  //     .catch((err) => console.log(err));
-  // }, []);
-  // useEffect(() => {
-  //   info = result;
-  // }, [result]);
+  const apiUrl = `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`;
 
+  let lat,lng
+  const validateIPv4 = (ip) => {
+    if (!ip) {
+      // Check for empty string or null/undefined
+      return false;
+    }
+    const regex =
+      /^([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])(\.([1-9]?\d|1\d\d|2[0-4]\d|25[0-5])){3}$/;
+    return regex.test(input);
+  };
+
+  const getIpData = () => {
+    // fetch(apiUrl)
+    //   .then(async (res) => await res.json())
+    //   .then(async (data) => {
+    //     await setResult(data);
+    //   })
+    //   .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getIpData();
+  }, []);
+  useEffect(() => {
+
+  }, [result]);
+
+  if(result){
+    lat = result.location.lat
+    lng = result.location.lng
+  }
+  else{
+    lat = 51.05
+    lng = -0.09
+  }
+  useEffect(() => {
+    if (validateIPv4(input)) {
+      setIpAddress(input);
+    } else {
+      setIpAddress("");
+    }
+  }, [input]);
   return (
     <>
       <div className="app w-[100vw] h-[100vh]">
@@ -40,26 +74,35 @@ function App() {
           </h1>
           <div className="input rounded-lg  flex justify-center mt-6">
             <input
-              type="number"
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+              max={14}
+              min={7}
+              value={input}
+              type="tel"
               className="bg-white p-2.5 w-[450px] rounded-tl-lg rounded-bl-lg"
             />
-            <button className="bg-[#2b2b2b] text-white px-4 rounded-tr-lg rounded-br-lg">
+            <button
+              onClick={getIpData}
+              className="bg-[#2b2b2b] text-white px-4 rounded-tr-lg rounded-br-lg"
+            >
               <FontAwesomeIcon
                 icon={faGreaterThan}
               />
             </button>
           </div>
-          <div className="info rounded-lg bg-white w-[80%] py-14 h-[50px] border p-3 m-auto mt-11 flex justify-around items-center">
+          <div className="inf rounded-lg bg-white w-[80%] py-14 h-[50px] p-3 m-auto mt-11 flex justify-around items-center ">
             <div>
               <h2>IP Address</h2>
-              <h1>{info ? info.ip : " "}</h1>
+              <h1>{result ? result.ip : " "}</h1>
             </div>
             <hr />
             <div>
               <h2>Location</h2>
               <h1>
-                {info
-                  ? info.location.region
+                {result && result.location
+                  ? result.location.region
                   : " "}
               </h1>
             </div>
@@ -67,23 +110,22 @@ function App() {
             <div>
               <h2>Timezone</h2>
               <h1>
-                {`UTC ${
-                  info
-                    ? info.location.timezone
-                    : " "
-                }`}
+                {result && result.location
+                  ? "UTC " +
+                    result.location.timezone
+                  : " "}
               </h1>
             </div>
             <hr />
             <div>
               <h2>ISP</h2>
-              <h1>{info ? info.isp : ""}</h1>
+              <h1>{result ? result.isp : ""}</h1>
             </div>
           </div>
         </div>
 
         <MapContainer
-          center={[51.505, -0.09]}
+          center={[lat, lng]}
           zoom={13}
           className="map"
         >
@@ -91,7 +133,7 @@ function App() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={[51.505, -0.09]}>
+          <Marker position={result ?[lat, lng] :[51.505, -0.09]}>
             <Popup>
               A pretty CSS3 popup. <br /> Easily
               customizable.
